9ee45c668c818f9e19749c086316d15ce83f880e
Ricard Figuls <ricard.figuls@gmail.com>
2019-07-30 19:48:12 +0200

    ADD DynControl reference

--- src/lib/MyFormControl.ts
+++ src/lib/MyFormControl.ts
@@ -1,14 +1,17 @@
 import { FormControl, ValidatorFn, AsyncValidatorFn } from '@angular/forms';
 import { ElementRef } from '@angular/core';
 import { AbstractControlOptions } from '@angular/forms/src/model';
+import { DynControl } from './dyn-control';
 
 export class MyFormControl extends FormControl {
   public element: ElementRef;
+  public control: DynControl;
 
   // tslint:disable-next-line:max-line-length
-  constructor(formState?: any, validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null, asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null, formElement?: ElementRef ) {
+  constructor(formState?: any, validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null, asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null, formElement?: ElementRef, dynControl?:DynControl ) {
     super(formState, validatorOrOpts, asyncValidator);
     this.element = formElement;
+    this.control = dynControl;
   }
 
   public focus() {
